9e862cf02ad12b073b6bd58db7d46d0a8b606d3b
Aristides Jaramillo <aristyjara506@gmail.com>
2023-09-23 14:36:40 -0500

    Flutter app quiz

---
+++ lib/data/questions.dart
@@ -0,0 +1,55 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    '¿Cuál es la duración de todo el procedimiento del lavado de manos?',
+    [
+      '40-60 seg',
+      '30-40 seg',
+      '60-90 seg',
+      '30-50 seg',
+    ],
+  ),
+  QuizQuestion('¿Cómo se llama a las emergencias, urgencias y la atención continua que se prestan fuera de los hospitales?', [
+    'Urgencias extrahospitalarias',
+    'Urgencias intrahospitalarias',
+    'Urgencias hospitalarias',
+    'Emergencias hospitalarias',
+  ]),
+  QuizQuestion(
+    '¿Qué evaluación clinica inmediata permite identificar y tratar lesiones de riesgo vital de forma rapida y organizada?',
+    [
+      'Atención inicial',
+      'Atención continua',
+      'Atención mediata',
+      'Atención inmediata',
+    ],
+  ),
+  QuizQuestion(
+    '¿Cuál condicion el paciente considera como prioritaria, pero que no pone en peligro su vida y su demora máxima para su atención es de 120 minutos?',
+    [
+      'Urgencia menor',
+      'Emergencia',
+      'Urgencia continua',
+      'Urgencia mayor',
+    ],
+  ),
+  QuizQuestion(
+    '¿Cuáles son las características de un RCP en adultos de alta calidad?',
+    [
+      '30 compresiones/2 respiraciones',
+      '15 compresiones/3 respiraciones',
+      '30 compresiones/3 respiraciones',
+      '15 compresiones/2 respiraciones',
+    ],
+  ),
+  QuizQuestion(
+    '¿Cuál es un antibiótico?',
+    [
+      'Eritromicina',
+      'paracetamol',
+      'ibuprofeno',
+      'acetaminofén',
+    ],
+  ),
+];
